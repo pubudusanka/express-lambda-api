b2e51a50e7b50fc4c1e5b997737b2c6cc086e871
Pubudu Sanka <pubudusanka79@gmail.com>
2025-01-19 11:14:58 +0530

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,7 +1,14 @@
 const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+const app = require('./app'); // Import the Express app
+
+// Create the server from the Express app
 const server = awsServerlessExpress.createServer(app);
 
+// Export the Lambda handler
 exports.handler = (event, context) => {
-awsServerlessExpress.proxy(server, event, context);
+    console.log('Event:', JSON.stringify(event));
+    console.log('Context:', JSON.stringify(context));
+
+    // Proxy the API Gateway event to the Express server
+    awsServerlessExpress.proxy(server, event, context);
 };

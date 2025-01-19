06579811b120f9e9effb63cde0fa216b639459da
Pubudu Sanka <pubudusanka79@gmail.com>
2025-01-19 11:12:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const routes = require('./routes'); // Import the routes
 
 // Begin Application config
 const app = express();
-app.use(bodyParser.json());
+
+// Middleware
+app.use(express.json()); // Use Express's built-in JSON parser
 
 // Mount routes under /api/v1/
 app.use('/api/v1/', routes);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
 
 module.exports = app;

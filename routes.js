06a9cc92029ba109dc2421e337af7694be154448
Pubudu Sanka <pubudusanka79@gmail.com>
2025-01-19 11:13:48 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+// Route: Root endpoint
 router.get('/', (req, res) => {
-res.json({ message: 'Hello from Express.js on AWS Lambda!' });
+    res.json({ message: 'Hello from Express.js on AWS Lambda!' });
 });
 
-router.get('/hello/:name', (req, res) => {
-const name = req.params.name;
-res.json({ message: `Hello, ${name}!` });
+// Route: Greet by name
+router.get('/hello/:name', (req, res, next) => {
+    try {
+        const name = req.params.name;
+        if (!name) throw new Error('Name parameter is required');
+        res.json({ message: `Hello, ${name}!` });
+    } catch (err) {
+        next(err); // Pass the error to the error handler
+    }
 });
 
-router.post('/data', (req, res) => {
-const data = req.body;
-res.json({ message: 'Received data successfully!', data });
+// Route: Receive data via POST
+router.post('/data', (req, res, next) => {
+    try {
+        const data = req.body;
+        if (!data) throw new Error('No data provided');
+        res.json({ message: 'Received data successfully!', data });
+    } catch (err) {
+        next(err); // Pass the error to the error handler
+    }
 });
 
 module.exports = router;
